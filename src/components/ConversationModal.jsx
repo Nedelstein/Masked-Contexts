21270f427d35fa832a5bba7a851e0bedf8bdaf0d
Noah Edelstein <Noedelstein@gmail.com>
2020-04-03 22:14:44 -0400

    added conversation modal component

---
+++ src/components/ConversationModal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ConversationModal = () => {
+  return (
+    <div>
+      <h1>This is working?</h1>
+    </div>
+  );
+};
+
+export default ConversationModal;
--- src/components/ConversationsMasonry.jsx
+++ src/components/ConversationsMasonry.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
 import Masonry from "react-masonry-css";
+import Modal from "react-modal";
+
+import ConversationModal from "../components/ConversationModal";
+
+const ModalStyle = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
+  }
+};
 
 let ConversationsMasonry = () => {
   //dynamically import all images from folder
@@ -14,7 +28,7 @@ let ConversationsMasonry = () => {
   const imagesDiv = new Array(images.length).fill().map((item, i) => {
     return (
       <div key={i}>
-        <div>
+        <div id={i}>
           <img src={images[i]} style={{ width: "60%" }} alt="whoops"></img>
         </div>
       </div>
@@ -28,15 +42,46 @@ let ConversationsMasonry = () => {
     500: 2
   };
 
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div Style="margin-top: 10%; margin-left: 10%">
+    <div style={{ marginLeft: "15%", marginTop: "15%" }}>
       <Masonry
         breakpointCols={breakpointColumbsObj}
         className="my-masony-grid"
         columnClassName="my-masonry-grid_column"
+        onClick={openModal}
       >
         {imagesDiv}
       </Masonry>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={ModalStyle}
+        contentLabel={"Conversation"}
+      >
+        {/* <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
+        <div>I am a modal</div> */}
+        <div ref={_subtitle => (subtitle = _subtitle)}>
+          <button onClick={closeModal}>close</button>
+
+          <ConversationModal />
+        </div>
+      </Modal>
     </div>
   );
 };
