67f2e7def6e29e6838fd62af9a25b639766f355e
Noah Edelstein <Noedelstein@gmail.com>
2020-04-07 22:49:29 -0400

    edited modal style

--- src/components/ConversationsMasonry.jsx
+++ src/components/ConversationsMasonry.jsx
@@ -1,25 +1,39 @@
 import React, { useState } from "react";
-import ReactDom from "react-dom";
+import { LinearCopy } from "gl-react";
+import { Surface } from "gl-react-dom";
 import Masonry from "react-masonry-css";
 import Modal from "react-modal";
+import GLTransition from "react-gl-transition";
+import GLTransitions from "gl-transitions";
+import timeLoop from "./timeLoop";
 
 import ConversationModal from "../components/ConversationModal";
 import conversations from "../conversations_lookup";
+// import { useHover } from "../hooks";
+
+const IMG = (imgName) => {
+  return require(`../assets/images/conversations/${imgName}`);
+};
 
 const ModalStyle = {
   content: {
     top: "50%",
-    left: "60%",
+    // left: "60%",
     right: "auto",
     bottom: "auto",
-    width: "100vw",
+    width: "80vw",
     // marginRight: "-50%",
     transform: "translate(-50%, -50%)",
-    backgroundColor: "gainsboro",
+    marginLeft: "50%",
+    backgroundColor: "#825B56",
     overflowY: "auto",
-    maxHeight: "80vh"
+    maxHeight: "80vh",
+    border: "none",
     // overflow
-  }
+  },
+  overlay: {
+    background: "rgba(86,126,130,0.3)",
+  },
 };
 
 const buttonStyle = {
@@ -30,7 +44,7 @@ const buttonStyle = {
   background: "white",
   color: "black",
   border: "2px solid black",
-  fontSize: "16px"
+  fontSize: "16px",
 };
 
 let ConversationsMasonry = () => {
@@ -38,6 +52,8 @@ let ConversationsMasonry = () => {
   let filename;
   const [modalIsOpen, setIsOpen] = useState(false);
   const [modal, setModal] = useState(null);
+  const [progress, setProgress] = useState(0);
+  // const [ref, hovered] = useHover();
 
   function importAll(r) {
     return r.keys().map(r);
@@ -55,8 +71,9 @@ let ConversationsMasonry = () => {
         <img
           id={filename}
           src={images[i]}
-          style={{ width: "60%" }}
+          style={{ width: "60%", objectFit: "cover", objectPosition: "center" }}
           alt="whoops"
+          // onMouseEnter={MasktoImg}
           onClick={openModal}
         ></img>
         {/* </div> */}
@@ -64,12 +81,54 @@ let ConversationsMasonry = () => {
     );
   });
 
-  const breakpointColumbsObj = {
-    default: 4,
-    1100: 3,
-    700: 3,
-    500: 2
-  };
+  // let transNum = 0;
+  // function transitionIn() {
+  //   console.log("mouse in the house");
+  //   transNum += 0.4;
+  //   console.log(transNum);
+  // }
+
+  // const increaseTransProgress = timeLoop(({ delay, duration, time }) => {
+  //   const index = Math.floor(time / (delay + duration));
+  //   const total = delay + duration;
+  //   const progress = (time - index * total - delay) / duration;
+  //   return progress;
+  // });
+
+  // const MasktoImg = () => {
+  //   const from = "masks/209842_mask.png";
+  //   const to = "original_imgs/000000209842.jpg";
+  //   // let progress = 0.2 + transNum;
+  //   const transition = GLTransitions[13];
+  //   return (
+  //     <GLTransition
+  //       from={IMG(from)}
+  //       to={IMG(to)}
+  //       progress={transProgress}
+  //       transition={transition}
+  //     />
+  //   );
+  // };
+
+  const MasktoImg = timeLoop(({ delay, duration, time }) => {
+    // console.log(time);
+    const from = "masks/209842_mask.png";
+    const to = "original_imgs/000000209842.jpg";
+    const index = Math.floor(time / (delay + duration));
+    const total = delay + duration;
+    const transition = GLTransitions[3];
+    const progress = (time - index * total - delay) / duration;
+    return progress > 0 ? (
+      <GLTransition
+        from={IMG(from)}
+        to={IMG(to)}
+        progress={progress}
+        transition={transition}
+      />
+    ) : (
+      <LinearCopy>{from}</LinearCopy>
+    );
+  });
 
   let activeContent;
   for (let i in conversations) {
@@ -85,7 +144,6 @@ let ConversationsMasonry = () => {
   }
 
   let subtitle;
-
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
     subtitle.style.color = "#f00";
@@ -93,6 +151,27 @@ let ConversationsMasonry = () => {
   function closeModal() {
     setIsOpen(false);
   }
+
+  const breakpointColumbsObj = {
+    default: 4,
+    1100: 3,
+    700: 3,
+    500: 2,
+  };
+
+  // let num = 0;
+  // let interval;
+  // const increaseTransProgress = () => {
+  //   console.log("mouse in the house");
+  //   // num = 0;
+  //   num += 0.1;
+  //   num = num;
+  //   console.log(num);
+  //   return num;
+  //   // console.log(num);
+  // };
+
+  // interval = () => setInterval(increaseTransProgress, 100);
 
   return (
     <div style={{ marginLeft: "15%", marginTop: "15%" }}>
@@ -104,14 +183,34 @@ let ConversationsMasonry = () => {
       >
         {imagesDiv}
       </Masonry>
+      <div>
+        <Surface
+          // ref={ref}
+          width={400}
+          height={200}
+          // onMouseEnter={() => {
+          // interval();
+          // setTransNum(increaseTransProgress);
+          // console.log(transProgress);
+          // }}
+          // onMouseLeave={() => {
+          // num = 0;
+          // setTransNum(0);
+          // }}
+        >
+          {/* <MasktoImg delay={2000} duration={1500} /> */}
+          {/* <MasktoImg /> */}
+        </Surface>
+      </div>
       <Modal
         isOpen={modalIsOpen}
         onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
         style={ModalStyle}
         contentLabel={"Conversation"}
+        closeTimeoutMS={300}
       >
-        <div ref={_subtitle => (subtitle = _subtitle)}>
+        <div ref={(_subtitle) => (subtitle = _subtitle)}>
           <button style={buttonStyle} onClick={closeModal}>
             Close
           </button>
--- src/hooks.js
+++ src/hooks.js
@@ -1,8 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const useOnClickOutside = (ref, handler) => {
   useEffect(() => {
-    const listener = event => {
+    const listener = (event) => {
       if (!ref.current || ref.current.contains(event.target)) {
         return;
       }
@@ -14,4 +14,28 @@ export const useOnClickOutside = (ref, handler) => {
       document.removeEventListener("mousedown", listener);
     };
   }, [ref, handler]);
+};
+
+export const useHover = () => {
+  // Reference to the element we're listen for events from
+  const ref = useRef();
+
+  // Hover state management
+  const [hovered, setHovered] = useState(false);
+
+  // Event handlers
+  const enter = () => setHovered(true);
+  const leave = () => setHovered(false);
+
+  // Simple effect, just bind and unbind the event handlers
+  useEffect(() => {
+    ref.current.addEventListener("mouseenter", enter);
+    ref.current.addEventListener("mouseleave", leave);
+    return () => {
+      ref.current.removeEventListener("mouseenter", enter);
+      ref.current.removeEventListener("mouseleave", leave);
+    };
+  }, [ref]);
+
+  return [ref, hovered];
 };
