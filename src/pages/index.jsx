440feb1c59489ac5aa1c95f490f159f635ec2e30
Noah Edelstein <Noedelstein@gmail.com>
2020-04-21 22:00:20 -0400

    added loading page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,12 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import LandingMasonry from "../components/LandingMasonry";
+import LoadingOverlay from "react-loading-overlay";
+import ScaleLoader from "react-spinners/ScaleLoader";
 
 const MainPage = () => {
   // const [open, setOpen] = useState(false);
   const node = useRef();
+  const [active, isActive] = useState(true);
   // const menuId = "main-menu";
   // useOnClickOutside(node, () => setOpen(false));
 
@@ -22,18 +25,48 @@ const MainPage = () => {
     top: "50%",
   };
 
+  const spinnerStyle = {
+    display: "block",
+    margin: "0 auto",
+    paddingBottom: "15px",
+  };
+
+  window.onload = () => {
+    isActive(false);
+  };
+
   return (
-    <div ref={node}>
-      <header className="App-header">
-        <div className="emptyBox">
-          <a href="/">
-            <div className="Title-text">Masked Contexts</div>
-          </a>
-        </div>
-        <p style={comingSoonStyle}>COMING SOON</p>
-        <LandingMasonry></LandingMasonry>
-      </header>
-    </div>
+    <LoadingOverlay
+      active={active}
+      spinner={<ScaleLoader css={spinnerStyle} size={100} color={"#fff"} />}
+      fadeSpeed={1000}
+      styles={{
+        overlay: (base) => ({
+          ...base,
+          background: "rgba(0,0,0,1)",
+        }),
+
+        content: (base) => ({
+          ...base,
+          fontFamily: "Big Caslon",
+          top: "50%",
+          left: "50%",
+        }),
+      }}
+      text="hang tight i'm loading"
+    >
+      <div ref={node}>
+        <header className="App-header">
+          <div className="emptyBox">
+            <a href="/">
+              <div className="Title-text">Masked Contexts</div>
+            </a>
+          </div>
+          <p style={comingSoonStyle}>COMING SOON</p>
+          <LandingMasonry></LandingMasonry>
+        </header>
+      </div>
+    </LoadingOverlay>
   );
 };
 
