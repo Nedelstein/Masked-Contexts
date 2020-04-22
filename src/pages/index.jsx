4bad8b6fa23e814e998f1381968f0992323afc78
Noah Edelstein <Noedelstein@gmail.com>
2020-04-21 21:53:34 -0400

    Added loading spinner to homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from "react";
+import LoadingOverlay from "react-loading-overlay";
+import ScaleLoader from "react-spinners/ScaleLoader";
 
-//components
-// import LandingText from "../components/LandingText";
 import LandingTypeWriter from "../components/LandingTypewriter";
 import Burger from "../components/Burger";
 import Menu from "../components/Menu";
@@ -11,27 +11,56 @@ import FocusLock from "react-focus-lock";
 
 const MainPage = () => {
   const [open, setOpen] = useState(false);
+  const [active, isActive] = useState(true);
   const node = useRef();
   const menuId = "main-menu";
   useOnClickOutside(node, () => setOpen(false));
 
+  window.onload = () => {
+    isActive(false);
+  };
+
+  const spinnerStyle = {
+    display: "block",
+    margin: "0, auto",
+  };
+
   return (
-    <div ref={node}>
-      <header className="App-header">
-        <div className="emptyBox">
-          <a href="/">
-            <div className="Title-text">Masked Contexts</div>
-          </a>
-        </div>
-        <LandingMasonry></LandingMasonry>
-        <FocusLock disabled={!open}>
-          <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
-          <Menu open={open} setOpen={setOpen} id={menuId} />
-        </FocusLock>
-        {/* <LandingText /> */}
-        <LandingTypeWriter />
-      </header>
-    </div>
+    <LoadingOverlay
+      active={active}
+      spinner={<ScaleLoader css={spinnerStyle} size={100} color={"#fff"} />}
+      fadeSpeed={1000}
+      styles={{
+        overlay: (base) => ({
+          ...base,
+          background: "rgba(0,0,0,1)",
+        }),
+
+        content: (base) => ({
+          ...base,
+          top: "50%",
+          left: "50%",
+        }),
+      }}
+      text="hang tight i'm loading"
+    >
+      <div ref={node}>
+        <header className="App-header">
+          <div className="emptyBox">
+            <a href="/">
+              <div className="Title-text">Masked Contexts</div>
+            </a>
+          </div>
+          <LandingMasonry></LandingMasonry>
+          <FocusLock disabled={!open}>
+            <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+            <Menu open={open} setOpen={setOpen} id={menuId} />
+          </FocusLock>
+          {/* <LandingText /> */}
+          <LandingTypeWriter />
+        </header>
+      </div>
+    </LoadingOverlay>
   );
 };
 
