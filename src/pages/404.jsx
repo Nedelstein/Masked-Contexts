f39e6648c5cd8f22fd37e7b35f4c9a9ba4ac4dc2
Noah Edelstein <Noedelstein@gmail.com>
2020-05-08 13:14:20 -0400

    fixed 404 page styles

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,12 +3,30 @@ import React from "react";
 const NotFoundPage = () => {
   return (
     <div>
-      <h2 style={{ marginLeft: "5%", color: "white" }}>
-        sowwwyyy we got the 404 not found!
+      <p
+        style={{
+          fontFamily: "RobotoMono",
+          fontSize: "16px",
+          marginLeft: "5%",
+          color: "black",
+        }}
+      >
+        404 not found! 404 not found! 404 not found! 404 not found! 404 not
+        found! 404 not found! 404 not found! 404 not found! 404 not found! 404
+        not found! 404 not found! 404 not found! 404 not found! 404 not found!
+        404 not found! 404 not found! 404 not found! 404 not found! 404 not
+        found! 404 not found! 404 not found! 404 not found! 404 not found! 404
+        not found! 404 not found! 404 not found! 404 not found! 404 not found!
+        404 not found! 404 not found! 404 not found! 404 not found! 404 not
+        found! 404 not found! 404 not found! 404 not found! 404 not found! 404
+        not found! 404 not found! 404 not found! 404 not found! 404 not found!
+        404 not found! 404 not found! 404 not found! 404 not found! 404 not
+        found! 404 not found! 404 not found! 404 not found! 404 not found! 404
+        not found! 404 not found! 404 not found! 404 not found! 404 not found!
         <br />
         <br />
-        i'm doing best
-      </h2>
+        i'm doing best ok?
+      </p>
     </div>
   );
 };
